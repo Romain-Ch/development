e9b0e0d34b3598063ec11c4cffa8c53a7850e335
Romain <rom__94@hotmail.fr>
2020-07-30 21:34:29 +0200

    final modifications

--- plotly/dash_plotly.py
+++ plotly/dash_plotly.py
@@ -12,17 +12,23 @@ import dash_html_components as html
 covid_monde = pd.read_csv(
     'https://raw.githubusercontent.com/owid/covid-19-data/master/public/data/owid-covid-data.csv',
     parse_dates=True,
+    low_memory=False,
 )
+
+covid_monde.continent.value_counts()
 
 covid_monde['time'] = pd.to_datetime(covid_monde['date'])
 covid_monde['week'] = (
     pd.to_datetime(covid_monde['time']).dt.strftime('%V').astype('int')
 )
 covid_monde['month'] = covid_monde['time'].dt.month
+covid_monde['abb_month'] = covid_monde['time'].dt.strftime('%b')
 
 covid_location = covid_monde[covid_monde['location'] != 'World'].sort_values(
     by=['location', 'time'], ascending=True
 )
+
+covid_location['continent'] = covid_location['continent'].str.lower()
 
 def top(ligne):
     val = ''
@@ -105,17 +111,16 @@ top_30_cases = pd.merge(
 
 covid_countries = (
     covid_location[covid_location['time'] > '2020-01-01']
-    .groupby(['location', 'iso_code', 'month'])[['new_cases', 'new_deaths']]
+    .groupby(['location', 'iso_code', 'continent', 'month', 'abb_month'])[['new_cases', 'new_deaths']]
     .sum()
     .reset_index()
 )
 
-# covid_location[(covid_location['location'] == 'France') & (covid_location['time'] <= '2020-07-07')].groupby('location')['new_tests'].sum()
 
 case_death_pop = pd.merge(
     (
         covid_location[covid_location['time'] > '2020-01-01']
-        .groupby(['location', 'month', 'population', 'life_expectancy'])[
+        .groupby(['location', 'month', 'abb_month', 'population', 'life_expectancy'])[
             ['new_cases', 'new_deaths']
         ]
         .sum()
@@ -140,8 +145,6 @@ case_death_pop['cases_per_pop'] = (
     case_death_pop['new_cases'] / case_death_pop['population']
 )
 
-# top_15_cases['month'] = top_15_cases['time'].dt.month
-
 # Initialize the App
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
@@ -149,6 +152,7 @@ app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 countries = top_30_cases['location'].unique()
 countries2 = agg_top_30['location'].unique()
 months = covid_countries['month'].unique()
+continent = covid_countries['continent'].unique()
 
 trace_1 = go.Scatter(
     x=top_30_cases[top_30_cases['location'] == 'United States']['cum_days'],
@@ -179,126 +183,114 @@ app.layout = html.Div(
     [
         html.Div(
             [html.H1('Dash pandemic situation')],
-            style={'margin-bottom': '50px', 'backgroundColor': '#ffffff'},
+            style={'margin-bottom': '50px', 'backgroundColor': '#F9F9F9'},
         ),
         html.Div(
             [
-                html.Div([
-                    dcc.Graph(id='line-filter-indicator', figure=line_fig),
-                    dcc.Graph(id='bar-filter-indicator')],
-                    style={'display':'inline-block'},
-                    className='six columns',
-                ),
-                html.Div([
-                    dcc.Graph(id='choropleth-map'),
-                    dcc.Graph(id='bubble-chart')],
-                    style={'display': 'inline-block'},
-                    className='six columns',
-                ),
-            ],
-            style={'display': 'inline-block'},
-        ),
-        html.Div(
-            [
-                html.P('Filter by location', className='control_label'),
-                dcc.Dropdown(
-                    id='countries-filter1',
-                    options=[{'label': i, 'value': i} for i in countries],
-                    value='United States',
-                    className='dcc_control',
-                ),
-                html.P('Select an indicator', className='control_label'),
-                dcc.Dropdown(
-                    id='selected-indicator',
-                    options=[
-                        {'label': 'cases', 'value': 'new_cases'},
-                        {'label': 'deaths', 'value': 'new_deaths'},
+                html.Div(
+                    [
+                        html.P('Select the country', className='control_label'),
+                        dcc.Dropdown(
+                            id='countries-filter1',
+                            options=[{'label': i, 'value': i} for i in countries],
+                            value='United States',
+                            className='dcc_control',
+                        ),
+                        html.P('Select an indicator', className='control_label'),
+                        dcc.Dropdown(
+                            id='selected-indicator',
+                            options=[
+                                {'label': 'cases', 'value': 'new_cases'},
+                                {'label': 'deaths', 'value': 'new_deaths'},
+                            ],
+                            value='new_deaths',
+                            className='dcc_control',
+                        ),
+                        html.P('Month of the year', className='control_label'),
+                        dcc.Slider(
+                            id='months-filter',
+                            value=covid_countries['month'].max(),
+                            min=covid_countries['month'].min(),
+                            max=covid_countries['month'].max(),
+                            marks={str(month): str(month) for month in months},
+                            className='dcc_control',
+                        ),
+
+                        html.P('Select the continent', className='control_label'),
+                        dcc.Dropdown(
+                            id='continent-filter',
+                            options=[
+                                {'label': 'World', 'value': 'world'},
+                                {'label': 'Africa', 'value': 'africa'},
+                                {'label': 'Asia', 'value': 'asia'},
+                                {'label': 'Europe', 'value': 'europe'},
+                                {'label': 'North America', 'value': 'north america'},
+                                {'label': 'South America', 'value': 'south america'},
+                            ],
+                            value='world',
+                            className='dcc_control',
+                        ),
                     ],
-                    value='new_deaths',
+                    style={'display': 'inline-block', 'width': '15%'},
+                    className='four columns',
+                    id='filter-options1',
                 ),
-                html.P('Select a period', className='control_label'),
-                dcc.Slider(
-                    id='months-filter',
-                    value=covid_countries['month'].max(),
-                    min=covid_countries['month'].min(),
-                    max=covid_countries['month'].max(),
-                    marks={str(month): str(month) for month in months},
+                html.Div(
+                    [
+                        html.Div(
+                            [dcc.Graph(id='line-filter-indicator', figure=line_fig),],
+                            className='five columns',
+                        ),
+                        html.Div(
+                            [dcc.Graph(id='choropleth-map')], className='five columns',
+                        ),
+                    ],
                 ),
             ],
-            style={
-                'display': 'inline-block',
-                'width': '33%',
-                'padding': '15px 10px 5px',
-            },
-            # className='two columns',
+            className='rows',
         ),
         html.Div(
             [
-                html.P('', className='control_label'),
-                dcc.Dropdown(
-                    id='countries-filter2',
-                    options=[{'label': i, 'value': i} for i in countries2],
-                    value='United States',
-                    className='dcc_control',
-                ),
-                html.P('Select an indicator', className='control_label'),
-                dcc.Dropdown(
-                    id='selected-indicator2',
-                    options=[
-                        {'label': '% cases', 'value': 'cases_per_pop'},
-                        {'label': '% deaths', 'value': 'deaths_per_pop'},
+                html.Div(
+                    [
+                        html.P('', className='control_label'),
+                        dcc.Dropdown(
+                            id='countries-filter2',
+                            options=[{'label': i, 'value': i} for i in countries2],
+                            value='United States',
+                            className='dcc_control',
+                        ),
+                        html.P('Select an indicator', className='control_label'),
+                        dcc.Dropdown(
+                            id='selected-indicator2',
+                            options=[
+                                {'label': '% cases', 'value': 'cases_per_pop'},
+                                {'label': '% deaths', 'value': 'deaths_per_pop'},
+                            ],
+                            value='cases_per_pop',
+                            className='dcc_control',
+                        ),
                     ],
-                    value='cases_per_pop',
+                    style={'display': 'inline-block', 'width': '15%'},
+                    className='four columns',
+                    id='filter-options2',
+                ),
+                html.Div(
+                    [
+                        html.Div(
+                            [dcc.Graph(id='bar-filter-indicator'),],
+                            className='five columns',
+                        ),
+                        html.Div(
+                            [dcc.Graph(id='bubble-chart'),], className='five columns',
+                        ),
+                    ],
                 ),
             ],
-            style={
-                'display': 'inline-block',
-                'width': '49%',
-            },
-            # className='two columns',
+            className='rows',
         ),
-        # html.Div(
-        #     [
-        #         html.P('Select an indicator', className='control_label'),
-        #         dcc.Dropdown(
-        #             id='selected-indicator2',
-        #             options=[
-        #                 {'label': '% cases', 'value': 'cases_per_pop'},
-        #                 {'label': '% deaths', 'value': 'deaths_per_pop'},
-        #             ],
-        #             value='cases_per_pop',
-        #         ),
-        #     ],
-        #     style={
-        #         'display': 'inline-block',
-        #         'width': '25%',
-        #         'margin-left': 'auto',
-        #         'margin-top': 'auto',
-        #     },
-        #     className='eight columns',
-        # ),
-        # html.Div(
-        #     [
-        #         html.P('Select a period', className='control_label'),
-        #         dcc.Slider(
-        #             id='months-filter',
-        #             value=covid_countries['month'].max(),
-        #             min=covid_countries['month'].min(),
-        #             max=covid_countries['month'].max(),
-        #             marks={str(month): str(month) for month in months},
-        #         ),
-        #     ],
-        #     style={
-        #         'display': 'inline-block',
-        #         'width': '25%',
-        #         'margin-left': 'auto',
-        #         'margin-bottom': 'auto',
-        #     },
-        #     className='ten columns',
-        # ),
     ],
     style={'display': 'flex', 'flex-direction': 'column'},
-    className='row',
 )
 
 
@@ -383,9 +375,10 @@ def upd_charts_plots(country_val):
     [
         dash.dependencies.Input('selected-indicator', 'value'),
         dash.dependencies.Input('months-filter', 'value'),
+        dash.dependencies.Input('continent-filter', 'value'),
     ],
 )
-def choropleth(selected_indic, month_val):
+def choropleth(selected_indic, month_val, continent_val):
 
     covid_countries['str_val_country'] = (
         covid_countries['location'] + ": " + covid_countries[selected_indic].apply(str)
@@ -418,6 +411,7 @@ def choropleth(selected_indic, month_val):
         title='Evolution of the pandemic situation in the world',
         autosize=True,
         hovermode='closest',
+        geo_scope=continent_val,
         plot_bgcolor='#F9F9F9',
         paper_bgcolor='#F9F9F9',
         margin=dict(l=20, r=0, b=20, t=40),
@@ -444,10 +438,10 @@ def choropleth(selected_indic, month_val):
 def upd_bubble_chart(selected_indic, country_val):
 
     color = [float(val) for val in case_death_pop.life_expectancy]
-    size = [val+val**4 for val in range(len(case_death_pop))]
+    size = [val + val ** 4 for val in range(len(case_death_pop))]
 
     data = go.Scatter(
-        x=case_death_pop[case_death_pop['location'] == country_val]['month'],
+        x=case_death_pop[case_death_pop['location'] == country_val]['abb_month'],
         y=case_death_pop[case_death_pop['location'] == country_val][selected_indic],
         mode='markers',
         marker=dict(sizemode='area', color=color, size=size),
